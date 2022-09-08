b5338d09b87696d7c70e3810ff5b2bfb39f6826a
Djibril NDAW <bondaw1502@gmail.com>
2022-09-08 11:49:15 +0000

    Betty coding style espace between puts and the parenthesis

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  * * Return: Always 0 (Success)
  * *my first C program
  * *printing a text using the function puts
- * */
+ */
 int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle" );
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
