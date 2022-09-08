2558fefebdbac58311c9f759a4c1e6ccc5216317
Djibril NDAW <bondaw1502@gmail.com>
2022-09-08 11:33:36 +0000

    task4 Hello, puts : first C program printing a text using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * *my first C program
+ * *printing a text using the function puts
+ * */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
