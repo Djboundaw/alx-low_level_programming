3c3ccca4df5346bf86121a7a3eaab03922248077
Djibril NDAW <bondaw1502@gmail.com>
2022-09-09 14:07:16 +0000

    task1 The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * betty style doc for function main goes there
+ * this program will tell if the stored number
+ * n is positive or negative
+ */
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n%10;
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %d is %d and is", n, lastDigit);
+		printf(" greater than 5\n");
+	}
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, lastDigit);
+	}
+	else if(lastDigit < 6 && lastDigit != 0)
+	{
+		printf("Last digit of %d is %d and is", n, lastDigit);
+		printf(" less than 6 and not 0\n");
+	}
+	return (0);
+}
