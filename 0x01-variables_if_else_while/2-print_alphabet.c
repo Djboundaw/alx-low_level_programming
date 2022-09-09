50a768c564903aa6ab677f4512b0074b70731ac0
Djibril NDAW <bondaw1502@gmail.com>
2022-09-09 14:37:01 +0000

    task2 I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * This program will print all the letters from a to z
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
+		printf("%c", letter);
+	}
+	printf("\n");
+	return (0);
+}
