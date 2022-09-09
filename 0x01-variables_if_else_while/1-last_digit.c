fb4bf2e0f391ab99d9190a9a5a537f4eb996a9a5
Djibril NDAW <bondaw1502@gmail.com>
2022-09-09 14:09:47 +0000

    task1 Betty coding style fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n%10;
+	lastDigit = n % 10;
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is", n, lastDigit);
@@ -24,9 +24,9 @@ int main(void)
 	}
 	else if (lastDigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, lastDigit);
+		printf("Last digit of %d is 0 and is 0", n);
 	}
-	else if(lastDigit < 6 && lastDigit != 0)
+	else if (lastDigit < 6 && lastDigit != 0)
 	{
 		printf("Last digit of %d is %d and is", n, lastDigit);
 		printf(" less than 6 and not 0\n");
