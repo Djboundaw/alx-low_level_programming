58efa57b31f8e32300327be28c807a1ef0ff18d3
Djibril NDAW <bondaw1502@gmail.com>
2022-09-10 02:17:31 +0000

    task12 Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+i * This program will print all possible
+ * combination of two two-digits regarding some conditions
+ */
+int main(void)
+{
+	int dgt1, dgt2, dgt3, dgt4;
+
+	for (dgt1 = 0; dgt1 <= 9; dgt1++)
+	{
+		for (dgt2 = 0; dgt2 <= 9; dgt2++)
+		{
+			for (dgt3 = 0; dgt3 <= 9; dgt3++)
+			{
+				if (dgt1 > dgt3)
+					continue;
+				for (dgt4 = 0; dgt4 <= 9; dgt4++)
+				{
+					if (dgt2 >= dgt4)
+						continue;
+					putchar(dgt1 + '0');
+					putchar(dgt2 + '0');
+					putchar(' ');
+					putchar(dgt3 + '0');
+					putchar(dgt4 + '0');
+					if (dgt1 == 9 && dgt2 == 8 && dgt3 == 9 && dgt4 == 9)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
