fc087d0d0af3f94a8d291a3eac8827af866414ca
Djibril NDAW <bondaw1502@gmail.com>
2022-09-11 18:30:05 +0000

    task12 finally have the solution

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,31 +10,21 @@ i * This program will print all possible
  */
 int main(void)
 {
-	int dgt1, dgt2, dgt3, dgt4;
+	int dgt1, dgt2;
 
-	for (dgt1 = 0; dgt1 <= 9; dgt1++)
+	for (dgt1 = 0; dgt1 <= 99; dgt1++)
 	{
-		for (dgt2 = 0; dgt2 <= 9; dgt2++)
+		for (dgt2 = dgt1 + 1; dgt2 <= 99; dgt2++)
 		{
-			for (dgt3 = 0; dgt3 <= 9; dgt3++)
-			{
-				if (dgt1 > dgt3)
-					continue;
-				for (dgt4 = 0; dgt4 <= 9; dgt4++)
-				{
-					if (dgt2 >= dgt4)
-						continue;
-					putchar(dgt1 + '0');
-					putchar(dgt2 + '0');
-					putchar(' ');
-					putchar(dgt3 + '0');
-					putchar(dgt4 + '0');
-					if (dgt1 == 9 && dgt2 == 8 && dgt3 == 9 && dgt4 == 9)
-						break;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(dgt1 / 10 + '0');
+			putchar(dgt1 % 10 + '0');
+			putchar(' ');
+			putchar(dgt2 / 10 + '0');
+			putchar(dgt2 % 10 + '0');
+			if (dgt1 == 98 && dgt2 == 99)
+				break;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	printf("\n");
