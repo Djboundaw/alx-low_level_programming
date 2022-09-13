5aeb7b8a69cdace227a86e405369c729f75d03f9
Djibril NDAW <bondaw1502@gmail.com>
2022-09-13 22:06:53 +0000

    task15 : sum of the Fibonacci numbers less than 4.000.000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * This program prints the sum of all the
+ * Fibonacci numbers starting with 1 to
+ * less than 4000000
  *
- * 
+ * Return: returns 0 (Success)
  */
 int main(void)
 {
+	int i, num1, num2, next_num, sum;
 
+	num1 = 1;
+	num2 = 2;
+	sum = 3;
+	next_num = num1 + num2;
+	sum = sum + next_num;
+	for (i = 1; i < 30; i++)
+	{
+		num1 = num2;
+		num2 = next_num;
+		next_num = num1 + num2;
+		sum = sum + next_num;
+	}
+	printf("%d\n", sum);
 	return (0);
 }
