27805e8606a6ba10164e1dfb8ab4532211547f4f
Djibril NDAW <bondaw1502@gmail.com>
2022-09-13 15:02:28 +0000

    i used the function _abs no trying a different way

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,8 @@ int print_last_digit(int c)
 	int l_digit;
 
 	if (c < 0)
-	{
-		c = c * -1;
-		l_digit = c % 10;
-	}
-	else
-		l_digit = c % 10;
+		c = -c;
+	l_digit = c % 10;
 	_putchar(l_digit + '0');
 	return (l_digit);
 }
