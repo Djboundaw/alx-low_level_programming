f9f04df41203b0e6ea68e7b3365c78234a088ee0
Djibril NDAW <bondaw1502@gmail.com>
2022-09-13 03:39:12 +0000

    a new project and this is the first task of current project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * This program will print __putchar
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This is the readme for a new project
+Project 0x02-functions_nested_loops
+learning C functions and nested loops
