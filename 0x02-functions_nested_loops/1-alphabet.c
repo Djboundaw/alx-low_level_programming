86257b8d452876c1db1d2c1f22bd1b7f330b599b
Djibril NDAW <bondaw1502@gmail.com>
2022-09-13 11:05:13 +0000

    task1 replacing the right content of the file 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,11 @@
  * This program will call the function putchar
  * to print the alphabet in lowercase
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	int i;
+	
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar(10);
 }
