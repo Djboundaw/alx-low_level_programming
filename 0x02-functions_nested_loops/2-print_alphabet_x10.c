ecb04749322804a71cca0d1f28b014981f5869a8
Djibril NDAW <bondaw1502@gmail.com>
2022-09-13 11:40:13 +0000

    task2 : betty style gets into trouble

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
  * This function will call 10 times
  * the function print_alphabet
  *
- * 
  */
 void print_alphabet_x10(void)
 {
 	int j = 10;
-
+	
 	while (j > 0)
 	{
 		int i;
+
 		for (i = 97; i <= 122; i++)
 			_putchar(i);
 		_putchar(10);
