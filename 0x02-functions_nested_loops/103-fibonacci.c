d36d93699d3818451e5328c39223a2806d6586d4
Djibril NDAW <bondaw1502@gmail.com>
2022-09-13 22:55:32 +0000

    task15 : sum of even-valued not all numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,27 +3,25 @@
 /**
  * main - Entry point
  *
- * This program prints the sum of all the
- * Fibonacci numbers starting with 1 to
- * less than 4000000
+ * This program prints the sum of Fibonacci
+ * numbers which are less than 4000000
  *
  * Return: returns 0 (Success)
  */
 int main(void)
 {
-	int i, num1, num2, next_num, sum;
+	int num1, num2, next_num, sum;
 
 	num1 = 1;
 	num2 = 2;
-	sum = 3;
-	next_num = num1 + num2;
-	sum = sum + next_num;
-	for (i = 1; i < 30; i++)
+	sum = num2;
+	while (next_num <= 4000000)
 	{
+		next_num = num1 + num2;
 		num1 = num2;
 		num2 = next_num;
-		next_num = num1 + num2;
-		sum = sum + next_num;
+		if (next_num % 2 == 0)
+			sum += next_num;
 	}
 	printf("%d\n", sum);
 	return (0);
