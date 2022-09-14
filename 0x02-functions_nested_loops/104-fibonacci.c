03be9311e046176f7914ef6da85efd90d5efbefa
Djibril NDAW <bondaw1502@gmail.com>
2022-09-14 02:28:55 +0000

    task16 : have no idea of how to fix it, this solution i've seen it somewhere

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -10,23 +11,40 @@
  */
 int main(void)
 {
-	int count;
-	unsigned long num1, num2, next_num;
+	int i = 0;
+	unsigned long a, b, c;
+	unsigned long a1, a2, b1, b2, c1, c2;
 
-	num1 = 1;
-	num2 = 2;
-	count = 2;
-	printf("%lu, %lu, ", num1, num2);
-	while (count != 98)
+	a = 0;
+	b = 1;
+	for (i = 0; i < 92; i++)
 	{
-		next_num = num1 + num2;
-		num1 = num2;
-		num2 = next_num;
-		count++;
-		if (count < 98)
-			printf("%lu, ", next_num);
-		else
-			printf("%lu\n", next_num);
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu, ", c);
 	}
+	a1 = a / 10000000000;
+	b1 = b / 10000000000;
+	a2 = a % 10000000000;
+	b2 = b % 10000000000;
+	for (i = 93; i <= 98; i++)
+	{
+		c1 = a1 + b1;
+		c2 = a2 + b2;
+		if (b1 + b2 > 9999999999)
+		{
+			c1 += 1;
+			c2 %= 10000000000;
+		}
+		printf("%lu%lu", c1, c2);
+		if (i != 98)
+			printf(", ");
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
+	}
+	printf("\n");
 	return (0);
 }
