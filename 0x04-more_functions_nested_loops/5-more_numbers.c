b13e0f68bfe29fe1f0c9f76f660655d1be6779a2
root <root@DESKTOP-5L8B8IK.localdomain>
2022-09-15 14:20:20 +0000

    task5 finally fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,21 @@
  */
 void more_numbers(void)
 {
-	int i, c;
+	int i;
+	char a, c;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (c = 0; c < 10; c++)
-			_putchar(c + '0');
-		for (c = 10; c < 15; c++)
+		for (a = 0; a < 15; a++)
 		{
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
+			c = a;
+			if (a >= 10)
+			{
+				_putchar(1 + '0');
+				c = a % 10;
+			}
+			_putchar(c + '0');
 		}
-
 		_putchar('\n');
 	}
 }
