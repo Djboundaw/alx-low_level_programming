4d727e3982fae13c9e98acf720ac7f4611b4cc11
Djboundaw <bondaw1502@gmail.com>
2022-09-16 19:03:39 +0000

    testing something

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,49 @@
 #include "main.h"
-
 /**
  * print_number - Entry point
- * @n: the number we will print using _putchar
+ * @n: parameter
  *
- * Return: void function no return value
+ * Return: nothing - void function
  */
 void print_number(int n)
 {
-	unsigned int a;
+	int a, b, max, min, i, j;
 
-	if (n < 0)
+	max = 2147483647, min = -2147483648;
+	i = 8, j = 7;
+	if (n == 0)
+		_putchar(n + '0');
+	else if (n > -100 && n < 100)
 	{
-		a = -n;
+		if (n < 0)
+		{
+			n = n * (-1);
+			_putchar(45);
+		}
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n > min && n < max)
+	{
+		if (n < 0)
+		{
+			n = n * (-1);
+			_putchar(45);
+		}
+		a = n / 10;
+		b = n % 10;
+		print_number(a);
+		_putchar(b + '0');
+	}
+	else if (n == min)
+	{
 		_putchar(45);
+		print_number(214748364);
+		_putchar(i + '0');
 	}
-	else
+	else if (n == max)
 	{
-		a = n;
+		print_number(214748364);
+		_putchar(j + '0');
 	}
-	if (a / 10 != 0)
-	{
-		print_number(a / 10);
-	}
-	_putchar(a % 10 + '0');
 }
