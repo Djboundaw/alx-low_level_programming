9eb6667e1243b3fbecb7aceb4d97525c507b8e27
Djboundaw <bondaw1502@gmail.com>
2022-09-16 16:37:30 +0000

    task12 new code using unsigned int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,43 +10,32 @@
  */
 void print_number(int n)
 {
-	int a, b;
+	unsigned int a;
 	
 	if (n == 0)
 	{
-		_putchar(n + '0');
+		_putchar(0 + '0');
 	}
-	else if (n > -100 && n < 100)
+	else
 	{
 		if (n < 0)
 		{
-			n = n * (-1);
+			a = -n;
 			_putchar(45);
 		}
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= -2147483647 && n <= 2147483646)
-	{
-		if (n < 0)
+		else
 		{
-			n = n * (-1);
-			_putchar(45);
+			a = n;
 		}
-		a = n / 10;
-		b = n % 10;
-		print_number(a);
-		_putchar(b + '0');
-	}
-	else if (n == INT_MIN)
-	{
-		_putchar(45);
-		print_number(214748364);
-		_putchar(8 + '0');
-	}
-	else if (n == INT_MAX)
-	{
-		print_number(214748364);
-		_putchar(7 + '0');
+		if (a < 100)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		else
+		{
+			print_number(a / 10);
+			_putchar(a % 10 + '0');
+		}
 	}
 }
