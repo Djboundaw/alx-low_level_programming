fb2090330f9d604794a2a400fe199e8bf93ef079
root <root@DESKTOP-5L8B8IK.localdomain>
2022-09-16 02:47:14 +0000

    task12 finally git add .git add .git add .git add .git add .git add .!

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * print_number - Entry point
+ * @n: the number we will print using _putchar
+ *
+ * Return: void function no return value
+ */
+void print_number(int n)
+{
+	int a, i, j;
+
+	a = 1, i = 0, j = 0;
+	if (n < 0)
+	{
+		n = -n;
+		_putchar(45);
+	}
+	while (n / a != 0)
+	{
+		a *= 10;
+		i++;
+	}
+	a /= 10;
+	while (j < i)
+	{
+		_putchar((n / a) % 10 + '0');
+		a /= 10;
+		j++;
+	}
+	if (n == 0)
+		_putchar(n + '0');
+
+}
