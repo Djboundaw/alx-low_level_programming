b1383d7c57a812bf37b2abe4df6d26c343d13618
Djboundaw <bondaw1502@gmail.com>
2022-09-19 14:40:30 +0000

    task9 : function that copy a string to another variable

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,30 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * *_strcpy - copy a string after another one
- * @dest: * before dest, first parameter
- * @src: * before src, second parameter
+ * *_strcpy - copy src string to dest string
+ * @dest: * before dest, destination where to copy the string
+ * @src: source of the string
  *
- * Return: void function no return value
+ * Return: dest the destination
  */
 char *_strcpy(char *dest, char *src)
 {
-	return (*dest);
+	int i, a, length;
+	char rev[500];
+	
+	i = 0, length = 0;
+	do {
+		a = *(src + length);
+		length++;
+	} while (a != '\0');
+	length -= 1;
+	for (i = 0; i < length; i++)
+	{
+		*(rev + i) = *(src + i);
+	}
+	for (i = 0; i < length; i++)
+	{
+		*(dest + i) = *(rev + i);
+	}
+	return (dest);
 }
