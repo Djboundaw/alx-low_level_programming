9ef13d57c9cd92b3464b3eae05936020f8deb137
Djboundaw <bondaw1502@gmail.com>
2022-09-19 19:21:13 +0000

    task10 : function that converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,5 +9,36 @@
  */
 int _atoi(char *s)
 {
-	retunr (0);
+	int nbr;
+	int a, b, length;
+	char i;
+
+	a = 0, length = 0;
+	do {
+		i = *(s + length);
+		if (i == 45)
+			a++;
+		if (i >= 48 && i <= 57)
+		{
+			b = i - '0';
+			break;
+		}
+		length++;
+	} while (i != '\0');
+	nbr = b;
+	length += 1;
+	do {
+		i = *(s + length);
+		if (i >= 48 && i <= 57)
+		{
+			b = i - '0';
+			nbr = nbr * 10 + b;
+		}
+		else
+			break;
+		length++;
+	} while (i != '\0');
+	if (a % 2 != 0)
+		nbr *= -1;
+	return (nbr);
 }
