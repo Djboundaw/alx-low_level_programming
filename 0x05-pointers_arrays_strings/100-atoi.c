eb4aed8d2d0ba986c9e9b728dd9d0c9dbcd94d36
Djboundaw <bondaw1502@gmail.com>
2022-09-19 09:18:00 +0000

    task8 : function that prints n elements of an array

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: * before s - string to convert
+ *
+ * Return: return the integer found
+ */
+int _atoi(char *s)
+{
+	retunr (0);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copy a string after another one
+ * @dest: * before dest, first parameter
+ * @src: * before src, second parameter
+ *
+ * Return: void function no return value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (*dest);
+}
