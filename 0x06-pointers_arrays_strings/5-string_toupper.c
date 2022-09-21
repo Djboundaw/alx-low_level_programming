a1faa6e3a0790831b36c8903a91f51e8bcdaa458
Djboundaw <bondaw1502@gmail.com>
2022-09-21 17:34:53 +0000

    task5: function that changes all lowercase letters to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,11 +3,27 @@
 /**
  * string_toupper - function that changes all
  * lowercase letters to uppercase
- * @s: * before s, the string
+ * @str: * before str, the string
  *
  * Return: return the new string
  */
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
-	return (s);
+	int i, a;
+	char tmp[500];
+	
+	i = 0;
+	do {
+		a = *(str + i);
+		if (a >= 97 && a <= 122)
+			a -= 32;
+		*(tmp + i) = a;
+		i++;
+	} while (a != '\0');
+	i = 0;
+	do {
+		*(str + i) = *(tmp + i);
+		i++;
+	} while (*(tmp + i) != '\0');
+	return (str);
 }
