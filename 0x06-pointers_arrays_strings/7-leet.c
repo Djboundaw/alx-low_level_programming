4714e8d44ed51aa9ea7c78fd9255137535aa1111
Djboundaw <bondaw1502@gmail.com>
2022-09-21 17:53:29 +0000

    task7: encoding a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,11 +2,35 @@
 
 /**
  * leet - function that encodes a string to 1337
- * @s: * before s, the string to encode
+ * @str: * before str, the string to encode
  *
  * Return: return the new encoding string
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	return (s);
+	int i, a;
+	char tmp[500];
+
+	i = 0;
+	do {
+		a = *(str + i);
+		if (a == 65 || a == 97)
+			a = 52;
+		else if (a == 69 || a == 101)
+			a = 51;
+		else if (a == 79 || a == 111)
+			a = 48;
+		else if (a == 84 || a == 116)
+			a = 55;
+		else if (a == 76 || a == 108)
+			a = 49;
+		*(tmp + i) = a;
+		i++;
+	} while (a != '\0');
+	i = 0;
+	do {
+		*(str + i) = *(tmp + i);
+		i++;
+	} while (*(tmp + i) != '\0');
+	return (str);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,9 +13,11 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
-char *string_toupper(char *);
+char *string_toupper(char *str);
 
-char *cap_string(char *);
+char *cap_string(char *str);
+
+char *leet(char *str);
 
 char *rot13(char *);
 
