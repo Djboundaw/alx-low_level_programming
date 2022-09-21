7722ad33cac717f28fcd0359597aad8aa8ba99ea
Djboundaw <bondaw1502@gmail.com>
2022-09-21 19:01:38 +0000

    task7: using two loops and one if statement

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,29 +8,20 @@
  */
 char *leet(char *str)
 {
-	int i, a;
-	char tmp[500];
+	int i, j, a;
+	char l_letters[] = {97, 101, 111, 116, 108};
+	char U_letters[] = {65, 69, 79, 84, 76};
+	char numbers[] = {52, 51, 48, 55, 49};
 
 	i = 0;
 	do {
 		a = *(str + i);
-		if (a == 65 || a == 97)
-			a = 52;
-		else if (a == 69 || a == 101)
-			a = 51;
-		else if (a == 79 || a == 111)
-			a = 48;
-		else if (a == 84 || a == 116)
-			a = 55;
-		else if (a == 76 || a == 108)
-			a = 49;
-		*(tmp + i) = a;
+		for (j = 0; j < 5; j++)
+		{
+			if (a == l_letters[j] || a == U_letters[j])
+				*(str + i) = numbers[j];
+		}
 		i++;
 	} while (a != '\0');
-	i = 0;
-	do {
-		*(str + i) = *(tmp + i);
-		i++;
-	} while (*(tmp + i) != '\0');
 	return (str);
 }
