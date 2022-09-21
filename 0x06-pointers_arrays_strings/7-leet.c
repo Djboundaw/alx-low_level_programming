7ac7f7973f7dcd88b9d00897c8acde67f696106b
Djboundaw <bondaw1502@gmail.com>
2022-09-21 04:26:05 +0000

    new project - all files initialized

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string to 1337
+ * @s: * before s, the string to encode
+ *
+ * Return: return the new encoding string
+ */
+char *leet(char *s)
+{
+	return (s);
+}
