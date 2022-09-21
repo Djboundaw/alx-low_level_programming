50e692cf6de096126a52bcb96c3a59a300d4da0f
Djboundaw <bondaw1502@gmail.com>
2022-09-21 16:16:24 +0000

    task3 : function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcmp - function that compares two strings
@@ -9,7 +10,20 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int res;
+	int i, res;
 
-	return (res);
+	i = 0, res = 0;
+	do {
+		if (*(s1 + i) < *(s2 + i))
+			res = res - 1;
+		else if (*(s1 + i) > *(s2 + i))
+			res = res + 1;
+		i++;
+	} while (*(s1 + i) != '\0' && *(s2 + i) != '\0');
+	if (res < 0)
+		return (-15);
+	else if (res > 0)
+		return (15);
+	else
+		return (0);
 }
