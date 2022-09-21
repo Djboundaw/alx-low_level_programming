813cae596f908252f9299b3cf36cf26578ea491f
Djboundaw <bondaw1502@gmail.com>
2022-09-21 12:03:46 +0000

    task0 : function that concatenates two string - a tmp must be used to store the second string otherwise it will be modified

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	return (dest)i;
+	int i, length1, length2;
+	char rev[1000], *tmp;
+	
+	i = 0, length1 = 0, length2 = 0;
+	tmp = src;
+	do {
+		*(rev + length1) = *(dest + length1);
+		length1++;
+	} while (*(dest + length1) != '\0');
+	do {
+		*(rev + length1 + length2) = *(tmp + length2);
+		length2++;
+	} while (*(tmp + length2) != '\0');
+	for (i = 0; i < length1 + length2; i++)
+	{
+		dest[i] = rev[i];
+	}
+	return (dest);
 }
