71d2f9e54d342355f35ea263a99f3eb378d2eca1
Djboundaw <bondaw1502@gmail.com>
2022-09-22 11:38:43 +0000

    task3 : still trying to fix it without knowing what's goes wrong

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,39 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcmp - function that compares two strings
  * @s1: * before s1, the first string
  * @s2: * before s2, the second string
  *
- * Return: return the result to dest
+ * Return: negative, positve or 0 for less than,
+ * greater than or equal
  */
 int _strcmp(char *s1, char *s2)
-{
-	int res, i;
+{int res, i, length1, length2;
 
-	i = 0, res = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		res += (s1[i] - s2[i]);
-		i++;
-	}
-	if (res < 0)
+	i = 0, res = 0, length1 = 0, length2 = 0;
+	do {
+		length1++;
+	} while (s1[length1] != '\0');
+	do {
+		length2++;
+	} while (s2[length2] != '\0');
+	if (length1 < length2)
 		res = -15;
-	else if (res > 0)
+	else if (length1 > length2)
 		res = 15;
+	else
+	{
+		while (s1[i] != '\0')
+		{
+			if (s1[i] != s2[i])
+				res += (s1[i] - s2[i]);
+			i++;
+		}
+		if (res < 0)
+			res = -15;
+		else if (res > 0)
+			res = 15;
+	}
 	return (res);
 }
