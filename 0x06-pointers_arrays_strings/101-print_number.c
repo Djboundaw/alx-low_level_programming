784c50b57336b1ae00a87f2ef464a1f4732f689f
Djboundaw <bondaw1502@gmail.com>
2022-09-22 04:03:57 +0000

    task9: function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,26 @@
 #include "main.h"
-
 /**
- * print_number - function that prints a number
- * @n: number to print
+ * print_number - Entry point
+ * @n: parameter
  *
- * Return: no return value - void function
+ * Return: nothing - void function
  */
 void print_number(int n)
 {
+	unsigned int a;
+
+	if (n < 0)
+	{
+		a = -n;
+		_putchar(45);
+	}
+	else
+	{
+		a = n;
+	}
+	if (a / 10 != 0)
+	{
+		print_number(a / 10);
+	}
+	_putchar(a % 10 + '0');
 }
