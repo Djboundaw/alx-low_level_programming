8cb20f64521a739d8f1717e4ee55080394618aec
Djboundaw <bondaw1502@gmail.com>
2022-09-22 12:21:43 +0000

    task3: think it works this time

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,31 +9,15 @@
  * greater than or equal
  */
 int _strcmp(char *s1, char *s2)
-{int res, i, length1, length2;
+{
+	int res, i;
 
-	i = 0, res = 0, length1 = 0, length2 = 0;
+	i = 0, res = 0;
 	do {
-		length1++;
-	} while (s1[length1] != '\0');
-	do {
-		length2++;
-	} while (s2[length2] != '\0');
-	if (length1 < length2)
-		res = -15;
-	else if (length1 > length2)
-		res = 15;
-	else
-	{
-		while (s1[i] != '\0')
-		{
-			if (s1[i] != s2[i])
-				res += (s1[i] - s2[i]);
-			i++;
-		}
-		if (res < 0)
-			res = -15;
-		else if (res > 0)
-			res = 15;
-	}
+		res = s1[i] - s2[i];
+		if (res != 0)
+			break;
+		i++;
+	} while (s1[i] != '\0' && s2[i] != '\0');
 	return (res);
 }
