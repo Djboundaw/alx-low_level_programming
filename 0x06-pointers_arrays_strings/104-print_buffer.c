d2062dc10abe59dc32eaeb8ce7217180e065e0d8
Djboundaw <bondaw1502@gmail.com>
2022-09-25 10:45:26 +0000

    task12 fixing

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_buffer - function that prints a buffer
@@ -9,4 +10,29 @@
  */
 void print_buffer(char *b, int size)
 {
+	int i, j, byte;
+
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x ", byte);
+		for (i = 0; i < 10; i++)
+		{
+			if (i + byte < size)
+				printf("%02x", b[i + byte]);
+			else
+				printf(" ");
+			if (i % 2 != 0)
+				printf(" ");
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (b[j + byte] >= 31 && b[j + byte] <= 126)
+				printf("%c", b[j + byte]);
+			else
+				printf(".");
+			if (j + byte >= size - 1)
+				break;
+		}
+		printf("\n");
+	}
 }
