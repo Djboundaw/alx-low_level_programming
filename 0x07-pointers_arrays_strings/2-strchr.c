f33e241580b7fc4eb5ef8dffd9c295bfd126ec41
Djboundaw <bondaw1502@gmail.com>
2022-09-26 20:26:47 +0000

    task2: return value for '\0'

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,17 +12,21 @@ char *_strchr(char *s, char c)
 	int i, occ;
 
 	i = 0, occ = -1;
-	while (*(s + i) != '\0')
-	{
+	do {
 		if (s[i] == c)
 		{
 			occ = i;
 			break;
 		}
 		i++;
-	}
-	if (occ < 0)
-		return ('\0');
+	} while (*(s + i) != '\0');
+	if (c == '\0')
+		return (s + i);
 	else
-		return (s + occ);
+	{
+		if (occ < 0)
+			return ('\0');
+		else
+			return (s + occ);
+	}
 }
