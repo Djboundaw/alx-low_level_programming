2da929a472f52fe53dcdc13c1abba9619d661c5f
Djboundaw <bondaw1502@gmail.com>
2022-09-26 22:07:12 +0000

    task4: function that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strspn - Function that gets the length of a prefix substring
+ * @s: the initial string
+ * @accept: string to searche for its characters on s
+ *
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n;
+	return (n);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that locates a character in a string
+ * @s: the string where we will search for the character
+ * @accept: the string to search for its bytes
+ *
+ * Return: retruns a pointer to first occurrence of any character of accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, length, occ, min;
+
+	i = 0, length = 0;
+	occ = -1;
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+	min = length;
+	do {
+		for (j = 0; j < length; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				occ = j;
+				break;
+			}
+		}
+		if (occ <= min)
+			min = occ;
+		i++;
+	} while (*(accept + i) != '\0');
+	if (occ >= 0)
+		return (s + min);
+	else
+		return ('\0');
+}
