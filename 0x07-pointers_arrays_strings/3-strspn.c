8af3f2d1567e7352a195ab4b8f6e0ec46f0af71e
Djboundaw <bondaw1502@gmail.com>
2022-09-27 02:50:30 +0000

    task3 : fixed

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,6 +9,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n;
-	return (n);
+	int i, j, count;
+
+	count = 0;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			break;
+	}
+	return (count);
 }
