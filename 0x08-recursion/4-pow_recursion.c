f8e5712408e932974e1c9f4fc3c8ac32be287e66
Djboundaw <bondaw1502@gmail.com>
2022-09-28 04:32:47 +0000

    task5: pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,5 +11,14 @@ int _pow_recursion(int x, int y)
 {
 	int pow;
 
+	if (y >= 0)
+	{
+		if (y == 0)
+			pow = 1;
+		else
+			pow = x * _pow_recursion(x, y - 1);
+	}
+	else
+		pow = -1;
 	return (pow);
 }
