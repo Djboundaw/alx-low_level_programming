ae4dd11c698306bef404cada65f62d853e61392b
Djboundaw <bondaw1502@gmail.com>
2022-09-28 05:37:59 +0000

    task0: puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,4 +8,19 @@
  */
 void _puts_recursion(char *s)
 {
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	if (length == 1)
+	{
+		_putchar(s[0]);
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		s = (s + 1);
+		_puts_recursion(s);
+	}
 }
