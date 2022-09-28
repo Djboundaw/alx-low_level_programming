a574a72a82a768c79df300d48db496ccad503b1c
Djboundaw <bondaw1502@gmail.com>
2022-09-28 04:24:18 +0000

    task3: factorial of n

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,10 +10,14 @@ int factorial(int n)
 {
 	int res;
 
-	if (n == 0)
-		res = 0;
-	else if (n == 1)
-		res = 1;
-	res = n * factorial(n - 1);
+	if (n < 0)
+		return (-1);
+	else
+	{
+		if (n == 0)
+			res = 1;
+		else
+			res = n * factorial(n - 1);
+	}
 	return (res);
 }
