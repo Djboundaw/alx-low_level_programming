88a5eed2fc899aa66ad3b1f65acf6e837af8290f
Djboundaw <bondaw1502@gmail.com>
2022-09-28 11:35:04 +0000

    task1: prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,10 +2,21 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: string to print
+ * @s: the string to print
  *
- * Return: void function - no return value
+ * Return: no return value - void function
  */
 void _print_rev_recursion(char *s)
 {
+	if (s[0] == '\0')
+		_putchar('\n');
+	else if (s[1] == '\0')
+	{
+		_putchar(s[0]);
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[0]);
+	}
 }
