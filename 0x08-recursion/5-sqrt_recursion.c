5049dac4760c37cb6012c40d097ac01d34a42e93
Djboundaw <bondaw1502@gmail.com>
2022-09-29 15:37:06 +0000

    task5: finally fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,32 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - calculate square root of a number
- * @n: number to return the square
+ * _sqrt_recursion - calculate square root of a number if integer
+ * @n: number to return the square root
  *
  * Return: -1 if n dosn't have square root
  */
 int _sqrt_recursion(int n)
 {
-	int res;
+	return (_square_recursion(n, 1));
+}
 
-	res = 1;
-	if (n == 0)
-		res = 0;
-	else if (n == 1)
-		res = 1;
-	else
+/**
+ * _square_recursion - calculate quare root of a number
+ * @n: number to returns its quare root
+ * @m: the square root of n
+ *
+ * Return: an integer
+ */
+int _square_recursion(int n, int m)
+{
+	if (m * m == n)
+		return (m);
+	else if (m * m < n)
 	{
-		if (n / res == res)
-			return (res);
-		else
-		{
-			res++;
-			_sqrt_recursion(n);
-		}
+		m += 1;
+		return (_square_recursion(n, m));
 	}
-	return (res);
+	else
+		return (-1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,6 +15,8 @@ int factorial(int n);
 
 int _pow_recursion(int x, int y);
 
+int _square_recursion(int n, int m);
+
 int _sqrt_recursion(int n);
 
 int is_prime_number(int n);
