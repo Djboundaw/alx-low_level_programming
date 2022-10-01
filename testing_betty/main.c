445c37545efc201b4c6001c473f50576f12fad02
Djboundaw <bondaw1502@gmail.com>
2022-10-01 00:18:44 +0000

    checking betty

---
+++ testing_betty/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: number of command line arguments
+ * @argv: array of size argc
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 2)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	printf("Error \n");
+	return (1);
+}
