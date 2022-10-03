2999942df48c8d3d93c9d0294819af99d3bbb83f
Djboundaw <bondaw1502@gmail.com>
2022-10-03 23:37:51 +0000

    task2 fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,13 @@ char *str_concat(char *s1, char *s2)
 	char *arr;
 	int size, i, len1, len2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	size = len1 + len2;
-	if (size == 0)
-		return ("");
 	arr = malloc(size * sizeof(char) + 1);
 	if (arr == 0)
 		return (NULL);
