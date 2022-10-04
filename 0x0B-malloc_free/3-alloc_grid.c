744023b65aad1fa5eb503c5b5ad63a8890cda245
Djboundaw <bondaw1502@gmail.com>
2022-10-04 01:03:39 +0000

    task3 not yet check9 and 10 failed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,14 +17,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	arr = malloc(height * sizeof(int *));
+	if (arr == 0)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == 0)
 			return (NULL);
-	}
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
