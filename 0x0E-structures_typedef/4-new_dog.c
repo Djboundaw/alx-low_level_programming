fb839f78765eab46eb0a366a3af15231d3fcc39c
Djboundaw <bondaw1502@gmail.com>
2022-10-10 21:29:24 +0000

    task4 - checker 7 : trying to free new_dog to fix the check

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = malloc(sizeof(char) * name_len + 1);
 	if (new_dog->name == NULL)
 	{
-		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
 	new_dog->name = strcpy(new_dog->name, name);
@@ -31,7 +31,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(sizeof(char) * own_len + 1);
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog->owner);
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
 	new_dog->owner = strcpy(new_dog->owner, owner);
