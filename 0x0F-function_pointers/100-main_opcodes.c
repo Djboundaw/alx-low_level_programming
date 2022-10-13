55da601d3105de18c5690943683ffe11861abd9e
Djboundaw <bondaw1502@gmail.com>
2022-10-13 02:22:32 +0000

    task4: advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if success another number otherwise
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
