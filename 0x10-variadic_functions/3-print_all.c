93abad5112b69effb7b68c42dbd2ba5583084d46
Djboundaw <bondaw1502@gmail.com>
2022-10-17 23:13:18 +0000

    task3: function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,55 @@
-#include "variadic_functions.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
- * print_all - print all parameters
- * @format: string of used formats
- *
- * Return: void function
- */
+* print_all - print char, integer, float and string
+* @format: format
+*/
 void print_all(const char * const format, ...)
 {
-	int j, i;
-	double f;
-	char *s;
-	char c;
-	va_list args;
+	va_list list;
+	unsigned int j = 0, start = 0;
+	char *p;
 
-	j = 0;
-	va_start(args, format);
+	va_start(list, format);
 	while (format && format[j] != '\0')
 	{
 		switch (format[j])
 		{
 			case 'c':
-				c = va_arg(args, int);
-				printf("%c", c);
+				switch (start)
+					case 1: printf(", ");
+				start = 1;
+				printf("%c", va_arg(list, int));
 				break;
 			case 'i':
-				i = va_arg(args, int);
-				printf("%d", i);
+				switch (start)
+					case 1: printf(", ");
+				start = 1;
+				printf("%i", va_arg(list, int));
 				break;
 			case 'f':
-				f = va_arg(args, double);
-				printf("%f", f);
+				switch (start)
+					case 1: printf(", ");
+				start = 1;
+				printf("%f", va_arg(list, double));
 				break;
-			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-					printf("(nil)");
-				printf("%s", s);
+			case's':
+				switch (start)
+					case 1: printf(", ");
+				start = 1;
+				p = va_arg(list, char*);
+				if (p)
+				{
+					printf("%s", p);
+					break;
+				}
+				printf("%p", p);
 				break;
 		}
-		if (format[j] == '\0')
-			break;
-		printf(", ");
 		j++;
 	}
-	va_end(args);
 	printf("\n");
+	va_end(list);
 }
