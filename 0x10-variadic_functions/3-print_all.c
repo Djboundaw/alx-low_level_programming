ea321941d6107e05a96b80c4627b00d58853f7d6
Djboundaw <bondaw1502@gmail.com>
2022-10-17 23:26:25 +0000

    task3 not yet

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	unsigned int j = 0, start = 0;
+	unsigned int j = 0;
 	char *p;
 
 	va_start(list, format);
@@ -20,36 +20,24 @@ void print_all(const char * const format, ...)
 		switch (format[j])
 		{
 			case 'c':
-				switch (start)
-					case 1: printf(", ");
-				start = 1;
 				printf("%c", va_arg(list, int));
 				break;
 			case 'i':
-				switch (start)
-					case 1: printf(", ");
-				start = 1;
 				printf("%i", va_arg(list, int));
 				break;
 			case 'f':
-				switch (start)
-					case 1: printf(", ");
-				start = 1;
 				printf("%f", va_arg(list, double));
 				break;
 			case's':
-				switch (start)
-					case 1: printf(", ");
-				start = 1;
+				printf(", ");
 				p = va_arg(list, char*);
 				if (p)
-				{
-					printf("%s", p);
-					break;
-				}
+				{ printf("%s", p);
+					break; }
 				printf("%p", p);
 				break;
 		}
+		printf(", ");
 		j++;
 	}
 	printf("\n");
