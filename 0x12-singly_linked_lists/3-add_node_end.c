55aeaee87ad46320aa200ce3222874aeb84ee459
Djboundaw <bondaw1502@gmail.com>
2022-10-21 02:43:39 +0000

    task3: add a node at the end of a linked kist

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node_end - add node at the end of a linked list
@@ -9,5 +10,26 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	return (p);
+	list_t *newName, *temp;
+	int len;
+
+	if (str == NULL)
+		return (NULL);
+	len = strlen(str);
+	newName = malloc(sizeof(list_t));
+	if (!newName)
+		return (NULL);
+	newName->len = len;
+	newName->str = strdup(str);
+	newName->next = NULL;
+	if (!*head)
+	{
+		*head = newName;
+		return (newName);
+	}
+	temp = *head;
+	while(temp->next)
+		temp = temp->next;
+	temp->next = newName;
+	return (newName);
 }
