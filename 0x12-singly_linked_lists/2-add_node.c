9a14fd3aaebc637abfaaa20a6c42cf0cd788c58b
Djboundaw <bondaw1502@gmail.com>
2022-10-21 02:20:11 +0000

    task2: add a new node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * add_node - adds a new node at the beginning of a linked list
@@ -9,5 +11,16 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	return (p);
+	list_t *newName;
+	int len;
+
+	len = strlen(str);
+	newName = malloc(sizeof(list_t));
+	if (!newName)
+		return (NULL);
+	newName->str = strdup(str);
+	newName->len = len;
+	newName->next = (*head);
+	(*head) = newName;
+	return (*head);
 }
