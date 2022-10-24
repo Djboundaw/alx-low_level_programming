c9f00e20188428bf18fbdafba24f8c74ba4d4c9b
Djboundaw <bondaw1502@gmail.com>
2022-10-24 18:54:48 +0000

    task2: adds new node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,5 +9,13 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	return ();
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(int));
+	if (!newNode)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = (*head);
+	(*head) = newNode;
+	return (*head);
 }
