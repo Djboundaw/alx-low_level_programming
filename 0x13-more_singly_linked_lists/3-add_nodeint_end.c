6f822505b4c2b74406cdf869fa1b1f8d25ee0b5e
Djboundaw <bondaw1502@gmail.com>
2022-10-24 19:01:30 +0000

    task3: add new node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,5 +9,21 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	return ();
+	listint_t *newNode, *temp;
+
+	newNode = malloc(sizeof(int));
+	if (!newNode)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = NULL;
+	if (!*head)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = newNode;
+	return (newNode);
 }
