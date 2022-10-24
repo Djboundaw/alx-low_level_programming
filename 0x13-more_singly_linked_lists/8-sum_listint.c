757e604a1958e3a776bca2622a09d9326faf9e14
Djboundaw <bondaw1502@gmail.com>
2022-10-24 22:03:34 +0000

    task8: prints the sum ofall the data of a list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,7 +8,17 @@
  */
 int sum_listint(listint_t *head)
 {
+	listint_t *temp;
 	int sum;
 
+	sum = 0;
+	if (head == NULL)
+		return (0);
+	temp = head->next;
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
 	return (sum);
 }
