0fea70c4e0a270ee9f402d6e5b81e1830c29b5d4
Djboundaw <bondaw1502@gmail.com>
2022-10-26 02:34:25 +0000

    task12 checker 8 still red

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,13 +17,13 @@ size_t print_listint_safe(const listint_t *head)
 		a = head - head->next;
 		n++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (a <= 0)
+		if (a > 0)
+			head = head->next;
+		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
-		else
-			head = head->next;
 	}
 	return (n);
 }
