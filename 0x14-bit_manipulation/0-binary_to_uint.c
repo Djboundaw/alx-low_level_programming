00d47913b41393ba66a0bd7c79c946b81af508d4
Djboundaw <bondaw1502@gmail.com>
2022-10-27 21:56:13 +0000

    task0: convert binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _pow - calculate power of un number
+ * @x: int we will power up
+ * @y: the power
+ *
+ * Return: x raised to the power of y
+ */
+int _pow(int x, int y)
+{
+	int pow;
+
+	if (y >= 0)
+	{
+		if (y == 0)
+			pow = 1;
+		else
+			pow = x * _pow(x, y - 1);
+	}
+	else
+		pow = -1;
+	return (pow);
+}
+
+/**
+ * binary_to_uint - converts a binary to unsigned int
+ * @b: binary in type string
+ *
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n, len;
+	int a, i;
+
+	n = 0;
+	len = strlen(b) - 1;
+	if (!b)
+		return (0);
+	for (i = len; i >= 0; i--)
+	{
+		if (b[i] > '1' || b[i] < '0')
+			return (0);
+		a = b[i] - '0';
+		n = n + a * _pow(2, len - i);
+	}
+	return (n);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+New project October 27
+Learning bit manipulation in C
