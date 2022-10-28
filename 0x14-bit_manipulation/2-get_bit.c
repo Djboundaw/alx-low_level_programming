c5390b57d919d876b9d73b05f492c0157b3dd43c
Djboundaw <bondaw1502@gmail.com>
2022-10-28 01:24:51 +0000

    task2: checker 7 red

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,10 +12,10 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int b;
 
 	b = n >> index;
+	if (!b)
+		return (-1);
 	if (b & 1)
 		return (1);
 	else if (!(b & 0))
 		return (0);
-	else
-		return (-1);
 }
