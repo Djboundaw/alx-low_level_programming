8f3b5cbbaf7f2404bf30be2cc768b32749965555
Djboundaw <bondaw1502@gmail.com>
2022-10-28 01:21:04 +0000

    task2: return the value of a bit at given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,31 +4,13 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int b, num;
-	int i, nbr;
+	unsigned long int b;
 
-	num = n;
-	nbr = 0;
-	i = 40;
-	while (i >= 0)
-	{
-		b = num >> i;
-		if (b & 1)
-			break;
-		i--;
-	}
-	while (i >= 0)
-	{
-			                b = n >> i;
-					                if (b & 1)
-								                {
-											                        _putchar('1');
-														                        nbr++;
-																	                }
-							                else if (nbr)
-										                        _putchar('0');
-									                i--;
-											        }
-        if (!nbr)
-	        _putchar('0');
+	b = n >> index;
+	if (b & 1)
+		return (1);
+	else if (!(b & 0))
+		return (0);
+	else
+		return (-1);
 }
