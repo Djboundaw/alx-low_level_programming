346886a9174ddcb59963337218a9dbec28fd0ad4
Djboundaw <bondaw1502@gmail.com>
2022-10-28 01:23:16 +0000

    task2: description for the function

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 /**
+ * get_bit - get the bit at a given index
+ * @n: the number to check
+ * @index: the index to return it's bit
+ *
+ * Return: value of a bit at index or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
