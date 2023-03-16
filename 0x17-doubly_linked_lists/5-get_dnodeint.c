049a41f9b4aeb7daec42bbdc3790916edb37d984
Djboundaw <bondaw1502@gmail.com>
2023-03-16 20:34:55 +0000

    task5 - Get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function entry point
+ * @head: head of linked list
+ * @index: index of the node to return
+ *
+ * Return: the nth node of linked list or NULL when failed
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	temp = head;
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+		if (temp->next == NULL)
+			return (NULL);
+	}
+
+	return (temp);
+}
