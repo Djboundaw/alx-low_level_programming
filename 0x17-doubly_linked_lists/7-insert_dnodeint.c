409960f1c6dfe03725f1d2b643d0e78bf8489133
Djboundaw <bondaw1502@gmail.com>
2023-03-16 21:53:06 +0000

    task7 - checker9 and 10 in red

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	temp = *h;
 	if (idx == 0)
-		return (add_dnodeint_end(h, n));
+	{
+		newNode = add_dnodeint_end(h, n);
+		return (newNode);
+	}
 
 	for (; idx != 1; idx--)
 	{
@@ -24,7 +27,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 	if (temp->next == NULL)
+	{
+		newNode = add_dnodeint_end(h, n);
 		return (add_dnodeint_end(h, n));
+	}
 
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
