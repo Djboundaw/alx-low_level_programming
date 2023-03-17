4d6a3d08fdba027d16e31bed000471a8b57abd8b
Djboundaw <bondaw1502@gmail.com>
2023-03-17 15:57:47 +0000

    task7 - adding a newNode at index 0 done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	temp = *h;
 	if (idx == 0)
-		newNode = add_dnodeint_end(h, n);
+		newNode = add_dnodeint(h, n);
 	else
 	{
 		for (; idx != 1; idx--)
