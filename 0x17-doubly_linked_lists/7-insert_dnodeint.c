bd53d3f5305e44ed2ee3990c2c39b703fb39ac39
Djboundaw <bondaw1502@gmail.com>
2023-03-17 16:02:41 +0000

    task7 - adding a newNode when idx = len(dlistint_t) + 1 fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,20 +25,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				return (NULL);
 		}
 		if (temp->next == NULL)
-		{
 			newNode = add_dnodeint_end(h, n);
-			return (add_dnodeint_end(h, n));
+		else
+		{
+			newNode = malloc(sizeof(dlistint_t));
+			if (newNode == NULL)
+				return (NULL);
+
+			newNode->n = n;
+			newNode->prev = temp;
+			newNode->next = temp->next;
+			temp->next->prev = newNode;
+			temp->next = newNode;
 		}
-
-		newNode = malloc(sizeof(dlistint_t));
-		if (newNode == NULL)
-			return (NULL);
-
-		newNode->n = n;
-		newNode->prev = temp;
-		newNode->next = temp->next;
-		temp->next->prev = newNode;
-		temp->next = newNode;
 	}
 
 	return (newNode);
