684f66daeeb5afa69bc4aeab67f51aeb70b4df20
Djboundaw <bondaw1502@gmail.com>
2023-04-11 01:39:06 +0000

    task0 - what's getting wrong

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *my_ht, unsigned int long i;
+	unsigned int long i;
+	hash_table_t *my_ht;
 
 	my_ht = malloc(sizeof(hash_table_t));
 	if (my_ht == NULL)
